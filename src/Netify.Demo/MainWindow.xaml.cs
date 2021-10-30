05c479818ba786f7f0063624e4e4feefa1314f21
AmRo <virtualemailbox045@gmail.com>
2021-10-30 12:19:00 +0330

    Create demo app

--- src/Netify.Demo/MainWindow.xaml.cs
+++ src/Netify.Demo/MainWindow.xaml.cs
@@ -1,8 +1,15 @@
-﻿namespace Netify.Demo
+﻿using System.ComponentModel;
+using System.Windows.Media;
+
+namespace Netify.Demo
 {
-    public partial class MainWindow : INetworkObserver
+    public partial class MainWindow : INetworkObserver, INotifyPropertyChanged
     {
         private readonly INetworkStatusNotifier _networkStatusNotifier;
+
+        private string? _connectionStatusText;
+        private SolidColorBrush? _connectionStatusBrush;
+        private ImageSource? _connectionStatusImage;
 
         public MainWindow(INetworkStatusNotifier networkStatusNotifier)
         {
@@ -10,11 +17,71 @@
             _networkStatusNotifier.AddObserver(this);
 
             InitializeComponent();
+            SetCurrentStatus();
+
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyname)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+
+        public string ConnectionStatusText
+        {
+            get => _connectionStatusText!;
+            set
+            {
+                _connectionStatusText = value;
+                OnPropertyChanged(nameof(ConnectionStatusText));
+            }
+        }
+
+        public SolidColorBrush ConnectionStatusBrush
+        {
+            get => _connectionStatusBrush!;
+            set
+            {
+                _connectionStatusBrush = value;
+                OnPropertyChanged(nameof(ConnectionStatusBrush));
+            }
+        }
+
+        public ImageSource ConnectionStatusImage
+        {
+            get => _connectionStatusImage!;
+            set
+            {
+                _connectionStatusImage = value;
+                OnPropertyChanged(nameof(ConnectionStatusImage));
+            }
         }
 
         public void ConnectivityChanged(ConnectivityStatus status)
+            => UpdateUI(status);
+
+        private void UpdateUI(ConnectivityStatus status)
         {
-            
+            Dispatcher.Invoke(() =>
+            {
+                if (status == ConnectivityStatus.Connected)
+                {
+                    ConnectionStatusText = Constants.ConnectedText;
+                    ConnectionStatusBrush = Constants.ConnectedBrush;
+                    ConnectionStatusImage = Constants.ConnectedImage;
+                }
+                else
+                {
+                    ConnectionStatusText = Constants.DisconnectedText;
+                    ConnectionStatusBrush = Constants.DisconnectedBrush;
+                    ConnectionStatusImage = Constants.DisconnectedImage;
+                }
+            });
+        }
+
+        private void SetCurrentStatus()
+        {
+            var currentStatus = _networkStatusNotifier.CheckNow();
+            UpdateUI(currentStatus);
         }
     }
 }
